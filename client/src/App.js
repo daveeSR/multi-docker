171ceb38432a0988e6a316c5fbf3ad6314329481
daveeSR <david.srowley@gmail.com>
2023-04-06 01:52:26 +0100

    testing deployment

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hello World! Let's All Learn React!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
